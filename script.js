3924321aa95448258373805250fae783af3cf449
Anna <predator190402@gmail.com>
2024-04-09 21:12:55 +0300

    правки

--- script.js
+++ script.js
@@ -1,112 +1,117 @@
-class View{
-    constructor(api) {
-        this.api = api;
+const URL = 'https://api.github.com/';
+const REPOSITORY_PER_PAGE = 5;
+
+class Api {
+    async loadRepositories(value) {
+        return await fetch(`${URL}search/repositories?q=${value}&per_page=${REPOSITORY_PER_PAGE}`)
+    }
+}
+
+class View {
+    constructor() {
         this.touch = document.querySelector('.touch')
         this.search = document.querySelector('.search')
+
         this.searchInput = this.createElement('input', 'search__input');
-        this.repositoryList = this.createElement('ul','repository__list');
+        this.repositoryList = this.createElement('ul', 'repository__list');
         this.searchList = this.createElement('ul', 'search__list')
-        this.touch.append(this.search)
-        this.search.append(this.searchInput)
-        this.search.append(this.searchList)
+
+        this.touch.append(this.search, this.searchInput, this.searchList, this.repositoryList)
     }
 
-    createElement(elementTag, elementClass){
+    createElement(elementTag, ...elementClass) {
         const element = document.createElement(elementTag)
-        if(elementClass){
-            element.classList.add(elementClass)
-        }
+        elementClass.forEach(el => element.classList.add(el))
         return element
     }
 
-    createRepository(repositoryData){
-        const repositoryElement = this.createElement('li','search__link');
+    createListElement(name, owner, stars) {
+        const element = this.createElement('li', 'repository__link')
+        element.textContent = `Name: ${name}\r\nOwner: ${owner}\r\nStars: ${stars}`
+
+        const closeButton = this.createElement('button', 'btn-close');
+        closeButton.addEventListener('click', function () {
+            this.parentElement.remove()
+        })
+
+        element.append(closeButton)
+        return element
+    }
+
+    createSearchElement(name) {
+        const element = this.createElement('div', 'search__item', 'search__link');
+        element.textContent = name
+        return element
+    }
+
+    createRepository(repositoryData) {
+        const repositoryElement = this.createSearchElement(repositoryData.name)
         repositoryElement.addEventListener('click', () => {
             let name = repositoryData.name;
             let owner = repositoryData.owner.login;
             let stars = repositoryData.stargazers_count;
-            this.searchList.innerHTML = ''
+            this.searchList.textContent = ''
             this.searchInput.value = ''
-            this.repositoryList.insertAdjacentHTML('beforeend', `<li class="repository__link">Name: ${name}<br>Owner: ${owner}<br>Stars: ${stars}<button class="btn-close"></button></li>`)
-
+            this.repositoryList.append(this.createListElement(name, owner, stars))
         })
-        this.touch.append(this.repositoryList)
-
-        repositoryElement.innerHTML = `<div class="search__item" data-owner="${repositoryData.owner.login}" data-stars="${repositoryData.stargazers_count}">${repositoryData.name}</div>`
         this.searchList.append(repositoryElement)
-
-        this.repositoryList.addEventListener("click", function (event) {
-            let target = event.target;
-            if (!target.classList.contains("btn-close")) {
-                return;
-            }
-            target.parentElement.remove();
-        });
     }
 }
 
-class Search{
+class Search {
     constructor(view, api) {
         this.view = view;
         this.api = api;
-        this.view.searchInput.addEventListener('keyup', this.debounce(this.searchRepository.bind(this), 500))
+        this.view.searchInput.addEventListener(
+            'input',
+            this.debounce(this.searchRepository.bind(this), 500)
+        )
     }
 
-    searchRepository(){
-        const searchValue = this.view.searchInput.value;
-        if(searchValue) {
-            this.clearRepositories()
-            this.repositoriesRequest(searchValue)
-        } else{
-            this.clearRepositories()
+    async searchRepository(event) {
+        this.clearRepositories()
+        const searchValue = event.target.value;
+        if (searchValue) {
+            await this.repositoriesRequest(searchValue)
         }
     }
 
-    async repositoriesRequest(searchValue){
+    async repositoriesRequest(searchValue) {
         try {
-            await this.api.loadRepositories(searchValue).then((res)=> {
-                if (res.ok){
-                    res.json().then(res =>{
-                        res.items.forEach(repository => this.view.createRepository(repository))
-                    })
-                }
-            })
+            const repos = await this.api.loadRepositories(searchValue)
+            const json = await repos.json()
+            json.items.forEach(el => this.view.createRepository(el))
         } catch (e) {
             console.log('Error: ' + e)
         }
     }
 
-    clearRepositories(){
-        this.view.searchList.innerHTML = '';
+    clearRepositories() {
+        this.view.searchList.textContent = ''
     }
 
-    debounce(func, wait, immediate){
-        let timeout;
-        return function (){
-            const context = this, args = arguments;
-            const later = function (){
-                timeout = null;
-                if(!immediate){
+    debounce(func, wait, immediate) {
+        let timeout
+        return function () {
+            const context = this
+            const args = arguments
+
+            const later = function () {
+                timeout = null
+                if (!immediate) {
                     func.apply(context, args)
                 }
             }
-            const callNow = immediate && !timeout;
-            clearTimeout(timeout);
-            timeout = setTimeout(later, wait);
-            if(callNow){
+            const callNow = immediate && !timeout
+            clearTimeout(timeout)
+
+            timeout = setTimeout(later, wait)
+            if (callNow) {
                 func.apply(context, args)
             }
         }
     }
 }
 
-const URL = 'https://api.github.com/';
-const REPOSITORY_PER_PAGE = 5;
-class Api{
-    async loadRepositories(value){
-         return await fetch(`${URL}search/repositories?q=${value}&per_page=${REPOSITORY_PER_PAGE}`)
-    }
-}
-
 const api = new Api()
-new Search(new View(api), api)
+new Search(new View(), api)
